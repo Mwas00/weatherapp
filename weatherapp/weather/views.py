b84587762432eca3dfbe54e9d7a2b6f13a6cd486
petern <95612311+Mwas00@users.noreply.github.com>
2025-01-15 05:45:21 +0000

    Add files via upload

---
+++ weatherapp/weather/views.py
@@ -0,0 +1,28 @@
+import requests
+from django.shortcuts import render
+
+def get_weather(request):
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        api_key = 'your_openweathermap_api_key'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+        
+        response = requests.get(url)
+        data = response.json()
+        
+        if data.get('cod') != 200:
+            error_message = "City not found. Please try again."
+            return render(request, 'weather/weather.html', {'error': error_message})
+        
+        weather_data = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'weather': data['weather'][0]['description'],
+            'humidity': data['main']['humidity'],
+            'pressure': data['main']['pressure'],
+            'wind_speed': data['wind']['speed'],
+        }
+
+        return render(request, 'weather/weather.html', {'weather': weather_data})
+    
+    return render(request, 'weather/weather.html')
